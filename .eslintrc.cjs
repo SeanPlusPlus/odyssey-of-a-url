1bb510e04d9759a941df6ccd4d4f6957d2675204
Sean Stephenson <sean.m.stephenson@disney.com>
2025-08-25 10:57:23 -0700

    eslint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'standard-with-typescript',
+    'plugin:prettier/recommended' // disables conflicting ESLint rules + runs Prettier as a rule
+  ],
+  env: {
+    es2023: true,
+    node: true,
+    browser: true
+  },
+  ignorePatterns: [
+    'node_modules/',
+    '.next/',
+    'dist/',
+    'build/',
+    'coverage/'
+  ]
+}
